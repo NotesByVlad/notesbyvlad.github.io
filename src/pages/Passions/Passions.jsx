ac8505baf459a72f2577ef959653587b8033fcc0
NotesByVlad <NotesByVlad@gmail.com>
2025-05-27 15:50:55 +0300

    Content and structure for passions page

--- src/pages/Passions/Passions.jsx
+++ src/pages/Passions/Passions.jsx
@@ -1,27 +1,16 @@
+import passionsData from './data/passionsData.json';
+
 export default function Passions() {
   return (
     <div className="space-y-8 max-w-4xl mx-auto px-4">
-      
-      <section>
-        <div className="bg-surface text-text -mx-4 px-4 py-8">
-          <h3 className="text-xl font-semibold mb-2">Music</h3>
-          <p>I love Music</p>
-        </div>
-      </section>
-      
-      <section>
-        <div className="bg-surface text-text -mx-4 px-4 py-8">
-          <h3 className="text-xl font-semibold mb-2">Painting</h3>
-          <p>I love Painting.</p>
-        </div>
-      </section>
-
-      <section>
-        <div className="bg-surface text-text -mx-4 px-4 py-8">
-          <h3 className="text-xl font-semibold mb-2">Reading</h3>
-          <p>I love Reading.</p>
-        </div>
-      </section>
+      {passionsData.map((item, index) => (
+        <section key={index}>
+          <div className="bg-surface -mx-4 px-4 py-8">
+            <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+            <p>{item.content}</p>
+          </div>
+        </section>
+      ))}
     </div>
   );
 }
