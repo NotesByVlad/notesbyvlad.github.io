d0ca613828dca555c33f1d0b3a2028cbefca71d3
NotesByVlad <NotesByVlad@gmail.com>
2025-05-31 17:51:09 +0300

    Added colors for themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        themeShadow: '4px 8px 8px var(--shadow-theme)',
+        themeShadowInset: '0 0 20px var(--shadow-theme) inset',
+      },
       colors: {
         background: 'var(--color-background)',
         text: 'var(--color-text)',
+        link: {
+          active: 'var(--color-link-active)',
+          hover: 'var(--color-link-hover)'
+        },
         button: {
           bg: 'var(--color-button-bg)',
           hover: 'var(--color-button-hover)',
